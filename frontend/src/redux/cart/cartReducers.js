0fa4c9da97081ef993e662704f77ee9faffdcb75
mohsen.zergani <mohsen.zergani@gmail.com>
2021-11-09 14:47:49 +0330

    inital

--- frontend/src/redux/cart/cartReducers.js
+++ frontend/src/redux/cart/cartReducers.js
@@ -1,32 +1,50 @@
-import CartActionTypes from "./cartTypes";
-import { addItemToCart, removeItemFromCart } from "./cartUtils";
-const INITIAL_STATE = {
-  cartItems: [],
-  shippingAddress: {},
-};
-export const cartReducer = (state = INITIAL_STATE, action) => {
+import {
+  CART_ADD_ITEM,
+  CART_REMOVE_ITEM,
+  CART_SAVE_SHIPPING_ADDRESS,
+  CART_SAVE_PAYMENT_METHOD,
+  CART_CLEAR_ITEMS,
+} from "../constants/cartConstants";
+
+export const cartReducer = (
+  state = { cartItems: [], shippingAddress: {} },
+  action
+) => {
   switch (action.type) {
-    case CartActionTypes.CART_ADD_ITEM:
+    case CART_ADD_ITEM:
+      const item = action.payload;
+
+      const existItem = state.cartItems.find((x) => x.product === item.product);
+
+      if (existItem) {
+        return {
+          ...state,
+          cartItems: state.cartItems.map((x) =>
+            x.product === existItem.product ? item : x
+          ),
+        };
+      } else {
+        return {
+          ...state,
+          cartItems: [...state.cartItems, item],
+        };
+      }
+    case CART_REMOVE_ITEM:
       return {
         ...state,
-        cartItems: addItemToCart(state.cartItems, action.payload),
+        cartItems: state.cartItems.filter((x) => x.product !== action.payload),
       };
-    case CartActionTypes.CART_REMOVE_ITEM:
-      return {
-        ...state,
-        cartItems: removeItemFromCart(state.cartItems, action.payload),
-      };
-    case CartActionTypes.CART_SAVE_SHIPPING_ADDRESS:
+    case CART_SAVE_SHIPPING_ADDRESS:
       return {
         ...state,
         shippingAddress: action.payload,
       };
-    case CartActionTypes.CART_SAVE_PAYMENT_METHOD:
+    case CART_SAVE_PAYMENT_METHOD:
       return {
         ...state,
         paymentMethod: action.payload,
       };
-    case CartActionTypes.CART_CLEAR_ITEMS:
+    case CART_CLEAR_ITEMS:
       return {
         ...state,
         cartItems: [],
